f396912bfd29fe9e31b93722787020c851e22d99
si4795 <suaibislam56@gmail.com>
2026-05-11 16:33:41 +0600

    Solved -- 231A

---
+++ Team.cpp
@@ -0,0 +1,26 @@
+// Problem Code: 231A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int p, v, t;
+        cin >> p >> v >> t;
+
+        if (p + v + t >= 2) {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
